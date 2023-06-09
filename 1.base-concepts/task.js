cef84a7831a9daea9c4230672eb2b13c3128d8ae
Matyunina-nastya <matyunina-nastena@mail.ru>
2023-06-10 02:06:46 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,13 +4,13 @@ function solveEquation(a, b, c) {
   let x;
   let y;
   let d = b**2 - 4*a*c;
-  if(d < 0){
+  if(d < 0) {
     return arr; 
-  } else if (d === 0){
+  } else if (d === 0) {
     x = -b/(2*a);
     arr.push(x);
   }
-else (d > 0){
+else if (d > 0) {
   x = (-b + Math.sqrt(d))/(2*a);
   y = (-b - Math.sqrt(d))/(2*a);
   arr.push(x, y);
@@ -21,10 +21,8 @@ else (d > 0){
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 let p = (percent / 100) / 12;
-let n = countMonths;
 let s = amount - contribution;
-let monthlyFee = s * (p + (p / (((1 + p)**n) - 1)));
-let total = monthlyFee * 12;
-return Number total.toFixed(2);
+let monthlyFee = s * (p + (p / (((1 + p)**countMonths) - 1)));
+let total = monthlyFee * countMonths;
+return Number(total.toFixed(2));
 }
-calculateTotalMortgage(10, 0, 50000, 12)
