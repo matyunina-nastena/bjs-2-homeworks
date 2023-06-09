1e3e04f7401b8517d06101204b006a90f9e35b5a
Matyunina-nastya <matyunina-nastena@mail.ru>
2023-06-10 01:09:56 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -17,3 +17,14 @@ else (d > 0){
 }
   return arr;
 }
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+let p = (percent / 100) / 12;
+let n = countMonths;
+let s = amount - contribution;
+let monthlyFee = s * (p + (p / (((1 + p)**n) - 1)));
+let total = monthlyFee * 12;
+return Number total.toFixed(2);
+}
+calculateTotalMortgage(10, 0, 50000, 12)
