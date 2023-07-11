061f40fa9e5e9587bcc97202f958dc695ce7d4c7
Matyunina-nastya <matyunina-nastena@mail.ru>
2023-07-11 15:06:20 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    let compare = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+    return compare;
+  };
 
-function getUsersNamesInAgeRange(users, gender) {
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter(user => user.gender === gender).reduce((acc, item, i, arr) => {
+        acc+=item.age;
+        if (i === arr.length - 1){
+            return (acc / arr.length);
+        }
+        return acc;
+    }, 0);
+    return result;
+};
+
+
   
-}
+  
